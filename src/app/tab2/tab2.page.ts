c571386e70e84f47c4765e37e60dc8e911b1487a
marcio-eduardo <65565066+marcio-eduardo@users.noreply.github.com>
2022-09-08 15:32:29 -0300

    Funções TS

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -3,10 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
-
   constructor() {}
 
+  doarAlimentos() {
+    window
+      .open(
+        'https://www.google.com/maps/search/?api=1&query=Ponto+Doacao+Alimento',
+        '_blank'
+      )
+      .focus();
+  }
+
+  doarRoupas() {
+    window
+      .open(
+        'https://www.google.com/maps/search/?api=1&query=Ponto+Doacao+Roupas',
+        '_blank'
+      )
+      .focus();
+  }
+
+  doarSangue() {
+    window
+      .open(
+        'https://www.google.com/maps/search/?api=1&query=Ponto+Doacao+Sangue',
+        '_blank'
+      )
+      .focus();
+  }
 }
