927ad5018552d700a49d967e7d53c5ea74f4ec88
marcio-eduardo <65565066+marcio-eduardo@users.noreply.github.com>
2022-09-08 16:22:59 -0300

    Criado Formulário

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,25 +1,31 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
 })
-export class Tab3Page {
-
+export class Tab3Page implements OnInit {
   form: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {}
 
-
-  constructor(
-    private formBuilder: FormBuilder
-  ) {}
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      nome: ['', [Validators.required]],
+      endereco: ['', [Validators.required]],
+    });
+  }
 
   abrirUrl(url: string) {
     window.open(url, '_blank').focus();
   }
   whats() {
     this.abrirUrl('https://api.whatsapp.com/send?phone=5521994074268&text=');
+  }
+
+  enviar() {
+    console.log('Enviar');
   }
 }
